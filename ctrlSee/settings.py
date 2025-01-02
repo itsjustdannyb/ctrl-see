cf4acdb31608e5536793226711f93d33615614ec
david-shina <arindedavid91@gmail.com>
2025-01-02 01:31:01 +0000

    Django Backend Project for the Text editor

--- ctrlSee/settings.py
+++ ctrlSee/settings.py
@@ -116,7 +116,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIR = [
-    os.path.join(BASE_DIR, 'static_files')
+    os.path.join(BASE_DIR, 'static')
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
